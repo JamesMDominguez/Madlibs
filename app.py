362d7d4c893d7c12500ec6bcd743a0af793988a2
James Dominguez <69368392+JamesMDominguez@users.noreply.github.com>
2025-07-10 14:36:47 -0700

    Refactor app.py to remove redundant print statements and define core functions for order processing and stock checking

--- app.py
+++ app.py
@@ -1,32 +1,25 @@
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
-print("This is my first Repo!!!!!")
+
+# --- Function Definition: order (Returns a Value) ---
+def order(item)
+    print(f"\n--- Calling order('{item}') ---")
+    order_number = 42  # Simulating an order number
+    return order_number  # This line is crucial for returning the value
+
+# --- Function Definition: check_stock (Performs an Action) ---
+def check_stock(order_item):
+    print(f"\n--- Calling check_stock('{order_item}') ---")
+    stock = True  # Simulating stock check
+    return stock  # This line is crucial for returning the stock status
+
+# --- Function Definition: get_daily_special (Returns a Value) ---
+def get_daily_special():
+    special = "Blueberry Muffin"
+    return special # This returns the value of the daily special
+
+# --- Function Definition: check_order_ready (Performs an Action) ---
+def check_order_ready(order_item):
+    print(f"--- Calling check_order_ready('{order_item}') ---")
+    return True  # Simulating order readiness check
+
+def what_is_your_name():
+    return "Coach Olivia"  # This returns the name of the co-teacher
---
+++ app2.py
@@ -0,0 +1,24 @@
+import random
+
+rand = random.randint(1, 10)
+
+
+
+RED = '\033[31m'
+GREEN = '\033[32m'
+BLUE = '\033[34m'
+YELLOW = '\033[33m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+BOLD = '\033[1m'     
+RESET = '\033[0m'
+
+# create print statements with multiline and different random colors
+print(RED+"Hello, World!")
+print(GREEN+"Hello, World!")
+print(BLUE+"Hello, World!")
+print(YELLOW+"Hello, World!")
+print(MAGENTA+"Hello, World!")
+print(CYAN+"Hello, World!")
+print(WHITE+"Hello, World!")
